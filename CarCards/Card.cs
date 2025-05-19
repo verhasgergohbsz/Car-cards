50449490e2fa89415f4ba5b3b41f5279cac3d6b0
verhasgergohbsz <verhas.gergo@hbsz.edu.hu>
2025-05-19 10:44:56 +0200

    some images added

---
+++ CarCards/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCards
+{
+    class Card
+    {
+        //id
+        public int Id { get; set; }
+
+        //random stuff
+        public string Marka { get; set; }
+        public string Tipus { get; set; }
+        public string Orszag { get; set; }
+        public string Uzemanyag { get; set; }
+        public int Evjarat { get; set; }
+        
+        //car datas
+        public int Vegsebesseg { get; set; }
+        public float Gyorsulas { get; set; }
+        public int Suly { get; set; }
+        public int Terfogat { get; set; }
+        public int Loero { get; set; }
+        public int Nyomatek { get; set; }
+
+    }
+}
