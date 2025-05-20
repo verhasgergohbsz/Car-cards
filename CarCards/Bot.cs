6c3c7a19bd68cfaadd00067a82e2c90d48265dfb
verhasgergohbsz <verhas.gergo@hbsz.edu.hu>
2025-05-20 13:50:43 +0200

    added some class

---
+++ CarCards/Bot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCards
+{
+    internal class Bot
+    {
+        public int Points { get; set; }
+        public List<Card> Cards { get; set; }
+    }
+}
---
+++ CarCards/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCards
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public List<Card> Cards { get; set; }
+    }
+}
